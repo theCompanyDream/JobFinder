d4d891b82ea03600c6dd57443f04d262d9f5b796
thecompanydream <tbrantleyii@gmail.com>
2016-05-27 11:48:07 -0400

    FInished generator function

--- src/config.py
+++ src/config.py
@@ -4,8 +4,16 @@ using a classic Caeser shift substitution (3 letter shift)"""
 import yaml
 
 
-#Locations = lambda x : lambda  y : "{0},{1}".format(x, y)
-
+def Dict_To_String_Generator(diction):
+    """
+        Generator Function that concats a val, key
+    """
+    if diction:
+        for key, val in [[v,k] for v,k in diction.items()]:
+            for city in val:
+                yield "{0},{1}".format(city, key)
+    else:
+        raise TypeError()
 
 def location(file):
     """
@@ -13,8 +21,22 @@ def location(file):
     """
     with open(file, 'r+') as stream:
         data = yaml.load(stream)
-        print(data)
 
+        for x in Dict_To_String_Generator(data['Locations']):
+            print(x)
+
+        for loc,key in data['Websites'].items():
+            data['token'] = key['key']
+            data['url'] = key['url']
+            print(loc)
+            print(key)
+
+        #print(data)
+
+class Request(object):
+    def __init__(self, file):
+        with open(file, 'r+') as stream:
+            self.data = yaml.load(stream)
 
 if __name__ == "__main__":
     location(r'C:\Users\company2\workspace\jobs\bin\request.yaml')
