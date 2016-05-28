edbbc41d61608b6ac3503f79af7b2048a71ff4c3
thecompanydream <tbrantleyii@gmail.com>
2016-05-28 10:07:43 -0500

    finshed loading of the config file

--- src/config.py
+++ src/config.py
@@ -1,7 +1,7 @@
 """This script prompts a user to enter a message to encode or decode
 using a classic Caeser shift substitution (3 letter shift)"""
 
-import yaml, itertools
+import yaml,itertools
 
 
 def Dict_To_String_Generator(diction):
@@ -9,25 +9,22 @@ def Dict_To_String_Generator(diction):
         Generator Function that concats a val, key
     """
     if diction:
-        for key, val in [[v,k] for v,k in diction.items()]:
+        for key, val in [[v, k] for v, k in diction.items()]:
             for city in val:
                 yield "{0},{1}".format(city, key)
     else:
         raise TypeError()
 
-def location(file):
+def parseFile(file):
     """
         Loads file and translates it into a generator function
     """
     with open(file, 'r+') as stream:
         data = yaml.load(stream)
-        locationList = [loc for loc in Dict_To_String_Generator(data['Locations'])]
-        zippedParameters = itertools.product(data,data['Jobs'], locationList)
-        for x in zippedParameters:
-            print(x)
-        
-
-
+        data['Locations'] = [loc for loc in Dict_To_String_Generator(data['Locations'])]
+        #zippedParameters = list(itertools.product(data['Jobs'], locationList))
+        return data.pop('Websites'), data
 
 if __name__ == "__main__":
-    location(r'C:\Users\company2\workspace\jobs\bin\request.yaml')
+    data = parseFile(r'C:\Users\company2\workspace\JobFinder\bin\request.yaml')
+    print(data)
